896c173365c9d34c99be6ee11c8cb5f6e0b7202a
Sha786il <shaheensayed2001@gmail.com>
2022-07-07 23:50:49 +0530

    faculty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DeptnameComponent } from './deptname/deptname.component';
 import { FacultyComponent } from './faculty/faculty.component';
+import { StudentsComponent } from './students/students.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DeptnameComponent,
-    FacultyComponent
+    FacultyComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
